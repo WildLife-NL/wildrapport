80673c34caa98ce9555de150c3aa8c000603e6f2
Erfan Alizada <erfanalizada6@gmail.com>
2025-03-24 10:12:46 +0100

    env file, colour class, font color class created. created assets and added logos

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Private constructor to prevent instantiation
+  AppColors._();
+
+  // Main colors
+  static const Color darkGreen = Color(0xFF1C4620);
+  static const Color brown = Color(0xFF5E3E27);
+  static const Color lightMintGreen = Color(0xFFF3FAF3);
+  static const Color offWhite = Color(0xFFFFFFFc);
+}
---
+++ lib/constants/app_text_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:wildrapport/constants/app_colors.dart';
+
+class AppTextTheme {
+  // Private constructor to prevent instantiation
+  AppTextTheme._();
+
+  static const TextTheme textTheme = TextTheme(
+    bodyLarge: TextStyle(color: AppColors.brown),
+    bodyMedium: TextStyle(color: AppColors.brown),
+    bodySmall: TextStyle(color: AppColors.brown),
+    titleLarge: TextStyle(color: AppColors.brown),
+    titleMedium: TextStyle(color: AppColors.brown),
+    titleSmall: TextStyle(color: AppColors.brown),
+    labelLarge: TextStyle(color: AppColors.brown),
+    labelMedium: TextStyle(color: AppColors.brown),
+    labelSmall: TextStyle(color: AppColors.brown),
+    displayLarge: TextStyle(color: AppColors.brown),
+    displayMedium: TextStyle(color: AppColors.brown),
+    displaySmall: TextStyle(color: AppColors.brown),
+    headlineLarge: TextStyle(color: AppColors.brown),
+    headlineMedium: TextStyle(color: AppColors.brown),
+    headlineSmall: TextStyle(color: AppColors.brown),
+  );
+}
