d9474dd321477149c13b11f0218a0f0664e5583d
Micahve <micah@familievaneverdingen.nl>
2025-04-01 14:16:16 +0200

    Worked on API Implementation for Login functionality 2

--- lib/interfaces/api_interface.dart
+++ lib/interfaces/api_interface.dart
@@ -1,7 +1,10 @@
 import 'package:wildrapport/models/user_model.dart';
+import 'package:wildrapport/models/animal_model.dart';
 
 abstract class ApiInterface {
   Future<Map<String, dynamic>> authenticate(
       String displayNameApp, String email);
   Future<UserModel> authorize(String email, String code);
+  Future<List<AnimalModel>> getAllSpecies();
+  Future<AnimalModel> getSpecies(String id);
 }
--- lib/providers/api_provider.dart
+++ lib/providers/api_provider.dart
@@ -4,14 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'package:wildrapport/interfaces/api_interface.dart';
-import 'package:wildrapport/models/user_model.dart';
 import 'package:wildrapport/providers/api_client.dart';
+
+/* Models */
+import 'package:wildrapport/models/user_model.dart';
+import 'package:wildrapport/models/animal_model.dart';
 
 
 class ApiProvider implements ApiInterface{
   final ApiClient client;
   ApiProvider(this.client);
   
+  /* Authentication & Authorization */
+
   @override
   Future<Map<String, dynamic>> authenticate(String displayNameApp, String email) async {
     http.Response response = await client.post(
@@ -62,4 +67,45 @@ class ApiProvider implements ApiInterface{
       throw Exception(json!["detail"]);
     }
   }
+
+  /* Species */
+
+  @override
+  Future<List<AnimalModel>> getAllSpecies() async {
+    http.Response response = await client.get(
+        'species/',
+        authenticated: true,
+      );
+
+      List<dynamic>? json;
+
+      if (response.statusCode == HttpStatus.ok) {
+        json = jsonDecode(response.body);
+        List<AnimalModel> species =
+        (json as List).map((e) => AnimalModel.fromJson(e)).toList();
+        return species;
+    } 
+    else {
+      throw Exception(json ?? "Failed to get all species");
+    }
+  }
+
+  @override
+  Future<AnimalModel> getSpecies(String id) async {
+    http.Response response = await client.get(
+      'species/$id/',
+      authenticated: true,
+    );
+
+    Map<String, dynamic>? json;
+
+    if (response.statusCode == HttpStatus.ok) {
+      json = jsonDecode(response.body);
+      AnimalModel species = AnimalModel.fromJson(json!);
+      return species;
+    } 
+    else {
+      throw Exception(json ?? "Failed to get species");
+    }
+  }  
 }
