3529bd1c8a90a000d0aef4e356901772578eb967
Micahve <micah@familievaneverdingen.nl>
2025-04-03 10:19:37 +0200

    added empty method for getting questionnaire by id, asking bas how to return filled in questionnaires

--- lib/api/questionaire_api.dart
+++ lib/api/questionaire_api.dart
@@ -1,9 +1,14 @@
 import 'package:wildrapport/api/api_client.dart';
 import 'package:wildrapport/interfaces/api/questionnaire_api_interface.dart';
+import 'package:wildrapport/models/api_models/questionaire.dart';
 
 class QuestionaireApi implements QuestionnaireApiInterface{
   final ApiClient client;
   QuestionaireApi(this.client);
 
-
+  @override
+  Future<Questionnaire> getQuestionnaireByID(String id) {
+    // TODO: implement getQuestionnaireByID
+    throw UnimplementedError();
+  }
 }
