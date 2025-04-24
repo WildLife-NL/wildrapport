4993bfe5d552da74bd6d4ed87b891b37b3776e7a
Erfan Alizada <erfanalizada6@gmail.com>
2025-04-24 11:12:06 +0200

    app bar removed from map screen

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wildrapport/providers/map_provider.dart';
-import 'package:wildrapport/widgets/app_bar.dart';
 
 class MapScreen extends StatelessWidget {
   final Widget mapWidget;
@@ -19,22 +18,10 @@ class MapScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            CustomAppBar(
-              leftIcon: Icons.arrow_back_ios,
-              centerText: title,
-              rightIcon: Icons.menu,
-              onLeftIconPressed: onBackPressed ?? () => Navigator.of(context).pop(),
-              onRightIconPressed: () {/* Handle menu */},
-            ),
-            Expanded(
-              child: mapWidget,
-            ),
-          ],
-        ),
+        child: mapWidget,
       ),
     );
   }
 }
+
 
