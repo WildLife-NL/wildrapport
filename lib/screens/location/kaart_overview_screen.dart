dd4f2fe9099e6ae7da9f7a93fa7882e6f23a6629
m1lsy <521945@student.fontys.nl>
2026-06-01 11:07:57 +0200

    Fix map legend names
    Closes #248

--- lib/screens/location/kaart_overview_screen.dart
+++ lib/screens/location/kaart_overview_screen.dart
@@ -1656,22 +1656,22 @@ class _KaartOverviewScreenState extends State<KaartOverviewScreen>
                                   ),
                                   _legendRow(
                                     const Color(0xFF00BFD8),
-                                    'Camera',
+                                    'Cameraval',
                                     icon: Icons.camera_alt,
                                   ),
                                   _legendRow(
                                     const Color(0xFFFF9100),
-                                    'Acoustic',
+                                    'Akoestische sensor',
                                     icon: Icons.graphic_eq,
                                   ),
                                   _legendRow(
                                     const Color(0xFFFE008E),
-                                    'Collar',
+                                    'Diergedragen sensor',
                                     badgeIcon: Icons.settings_remote,
                                   ),
                                   _legendRow(
                                     const Color(0xFF0078DA),
-                                    'Aanrijding',
+                                    'Dieraanrijding',
                                   ),
                                   _legendRow(
                                     const Color(0xFF008C7B),
